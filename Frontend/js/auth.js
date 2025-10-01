35aae7f5c683d920207be9328cde3c0290f43cd7
gordoPlaTo <arielivanamor@gmail.com>
2025-10-01 19:55:00 -0300

    Adjunto Cambios en js (comienzo de login) y registro

--- Frontend/js/auth.js
+++ Frontend/js/auth.js
@@ -10,3 +10,52 @@ function togglePassword(fieldID, btn) {
         btn.textcontent = "👁️";
     }
 }
+
+//login
+
+const API_URL = "http://localhost:8080/...";
+
+document.addEventListener("DOMContentLoaded", () => {
+    const loginForm = document.getElementById("loginForm");
+
+    if (loginForm) {
+        loginForm.addEventListener("submit", async (e) => {
+            e.preventDefault();
+            const data = {
+                email: document.getElementById("email").value;
+                password: document.getElementById("password").value;
+            };
+
+            try {
+                const res = await fetch(`${API_URL}/login`, {
+                    method: "POST",
+                    headers: { "Content-Type": "application/json" },
+                    body: JSON.stringify(data)
+                });
+
+                if (!res.ok) {
+                    alert("Credenciales incorrectas");
+                    return;
+                }
+
+                const result = await res.json();
+
+                //guaramos token y rol en localstorage
+                localStorage.setItem("email", result.email);
+                localStorage.setItem("message", result.message);
+                localStorage.setItem("token", result.token);
+
+                //redirigimos dependiendo del rol
+                if (result.role === "ADMIN") {
+                    window.location.href = "dashboard.html";
+                } else {
+                    window.location.href = "index.html";
+                }
+            } catch (err) {
+                console.error("Error de conexión", err);
+                alert("Error al conectarse con el servidor")
+            }
+
+        });
+    }
+});
